107712a80354867c2fd2c0a2f04933e9ca27db1a
MarianoDesivo <79780807+MarianoDesivo@users.noreply.github.com>
2022-11-24 11:35:37 -0300

    Added services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeroesComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,11 +10,16 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   selectedHero: Hero;
-  heroes = HEROES;
-  constructor() {}
+  heroes: Hero[];
+  constructor(private heroService: HeroService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getHeroes();
+  }
 
+  getHeroes(){
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
   onSelect(hero: Hero) {
     this.selectedHero = hero;
     console.log(hero);
